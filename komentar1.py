ba5833063077e80bac9f391edffef882c325847a
Dude Arrasyid <dudearra@gmail.com>
2025-04-28 20:33:42 +0700

    Add files via upload

---
+++ komentar1.py
@@ -0,0 +1,23 @@
+#Ini hanya komentar yang kubuat
+"""
+Penulisan dari sebuah kutipan biasa yang dibuat secara cepat
+Hanya 4 baris biasa
+"""
+
+print ("jika ingin melakukan sesuatu kita harus berpikir agar tidak ada yang dirugikan dan dijatuhkan martabatnya")
+"""
+print("Sebelum mengenal dan mempelajari kita harus mengetahui apakah ada manfaat yang kita dapat")
+"""
+#nama penulis
+print ('Raiki Uchiha')
+
+#tanggal penulisan 
+print ('19-10-2024')
+
+#nama lain
+nama = "Reikara"
+print(nama)
+
+umur = '18 Tahun'
+
+print (umur)
