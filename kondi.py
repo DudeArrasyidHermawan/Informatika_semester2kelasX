990fa13408e1ebf1f1484e6ffdaf7213ad12fd78
Dude Arrasyid <dudearra@gmail.com>
2025-05-05 10:53:00 +0700

    Kondisi

---
+++ kondi.py
@@ -0,0 +1,25 @@
+
+kelu= int(input("Nilai Raport: "))
+if kelu == 10:
+    print("Anda dinyatakan Lulus")
+elif kelu >=5:
+    print("Anda dinyatakan masuk semester selanjutnya")
+else:
+    print("Anda dinyatakan Tidak naik semester")
+
+
+nilai = int(input("Nomor yang anda masukkan: ")) 
+if nilai  >= 90:
+    print ("Anda mendapatkan nilai A")
+
+elif nilai >= 85:
+    print ("Anda mendapatkan nilai B")
+   
+elif nilai >= 75:
+    print ("Anda mendapatkan nilai C")
+
+elif nilai >= 60:
+    print ("Anda mendapatkan nilai D")
+
+else :
+    print ("Anda mendapatkan nilai E") 
